10ee48e617686f24cb83fe7599e47013d0b948ce
Suriya005 <45837558+Suriya005@users.noreply.github.com>
2020-08-26 23:53:53 +0700

    Add files via upload

---
+++ web_Php/nick_04.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<style>
+    body {font-family: "Lato", sans-serif;}
+    .mySlides {display: none}
+    .span{hight : 1000px;}
+    table {
+        width: 100%;
+         height: 500px;
+          }
+</style>
+</head>
+<body>
+<br>
+<div class="w3-top">
+  <div class="w3-bar w3-black w3-card">
+    <a href="index.php" class="w3-bar-item w3-button w3-padding-large">หน้าแรก</a>
+    <a href="one.php" class="w3-bar-item w3-button w3-padding-large w3-hide-small">PHPinfo</a>
+    <a href="two.php" class="w3-bar-item w3-button w3-padding-large w3-hide-small">Pyramid</a>
+    <a href="three.php" class="w3-bar-item w3-button w3-padding-large w3-hide-small">iframe</a>
+  </div>
+</div>
+<pre style="font-family:TLWGTypewriter;font-size:20px;">
+        <?php echo "<br/>";
+            for ($nick=1; $nick <= 4 ; $nick++)
+            {
+             
+                 for ($suriya=1; $suriya <= $nick; $suriya++)
+                {
+                    echo "*";
+                }
+                 for ($suriya=$nick; $suriya >= 2; $suriya--)
+                {
+                    echo $suriya;
+                }
+                for ($suriya=1; $suriya <= $nick; $suriya++)
+                {
+                    echo $suriya;
+                }
+            echo "\n";
+            }
+        ?>
+    </pre>
+    <table></table>
+<footer class="w3-container w3-padding-64 w3-center w3-opacity w3-light-grey w3-xlarge">
+  <a href="https://www.facebook.com/Nick.Godzilla?ref=bookmarks"><i class="fa fa-facebook-official w3-hover-opacity"></i></a>
+  <a href="https://www.instagram.com/nickkybeginner/?hl=th"><i class="fa fa-instagram w3-hover-opacity"></i></a>
+  <a href="https://www.youtube.com/channel/UC7RFNDhNZD4x_UvToRIRNmA"><i class="fa fa-youtube w3-hover-opacity"></i></a>
+  <a href="https://twitter.com/55120Titan"><i class="fa fa-twitter w3-hover-opacity"></i></a>
+</footer>
+</body>
+</html>
+    
---
+++ web_Php/two.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<style>
+    body {font-family: "Lato", sans-serif;}
+    .mySlides {display: none}
+    .span{hight : 1000px;}
+    table {
+        width: 100%;
+         height: 250px;
+          }
+
+</style>
+</head>
+<body>
+<br>
+<div class="w3-top">
+  <div class="w3-bar w3-black w3-card">
+    <a href="index.php" class="w3-bar-item w3-button w3-padding-large">หน้าแรก</a>
+    <a href="one.php" class="w3-bar-item w3-button w3-padding-large w3-hide-small">PHPinfo</a>
+    <a href="two.php" class="w3-bar-item w3-button w3-padding-large w3-hide-small">Pyramid</a>
+    <a href="three.php" class="w3-bar-item w3-button w3-padding-large w3-hide-small">iframe</a>
+  </div>
+</div>
+
+    <table>
+    <br>
+    <br>
+    <a href="nick_01.php"class="w3-bar-item w3-button w3-padding-large">01</a>
+    <a href="nick_02.php"class="w3-bar-item w3-button w3-padding-large">02</a>
+    <a href="nick_03.php"class="w3-bar-item w3-button w3-padding-large">03</a>
+    <a href="nick_04.php"class="w3-bar-item w3-button w3-padding-large">04</a>
+    <a href="nick_05.php"class="w3-bar-item w3-button w3-padding-large">05</a><br>
+    <a href="nick_06.php"class="w3-bar-item w3-button w3-padding-large">06</a>
+    <a href="nick_07.php"class="w3-bar-item w3-button w3-padding-large">07</a>
+    <a href="nick_08.php"class="w3-bar-item w3-button w3-padding-large">08</a>
+    <a href="nick_09.php"class="w3-bar-item w3-button w3-padding-large">09</a>
+    <a href="nick_10.php"class="w3-bar-item w3-button w3-padding-large">10</a><br>
+    <a href="nick_11.php"class="w3-bar-item w3-button w3-padding-large">11</a>
+    <a href="nick_12.php"class="w3-bar-item w3-button w3-padding-large">12</a>
+    <a href="nick_13.php"class="w3-bar-item w3-button w3-padding-large">13</a>
+    <a href="nick_14.php"class="w3-bar-item w3-button w3-padding-large">14</a>
+    <a href="nick_15.php"class="w3-bar-item w3-button w3-padding-large">15</a><br>
+    <a href="nick_16.php"class="w3-bar-item w3-button w3-padding-large">16</a>
+    <a href="nick_17.php"class="w3-bar-item w3-button w3-padding-large">17</a>
+    <a href="nick_18.php"class="w3-bar-item w3-button w3-padding-large">18</a>
+    <a href="nick_19.php"class="w3-bar-item w3-button w3-padding-large">19</a>
+    <a href="nick_20.php"class="w3-bar-item w3-button w3-padding-large">20</a><br>
+    <a href="nick_21.php"class="w3-bar-item w3-button w3-padding-large">21</a>
+    <a href="nick_22.php"class="w3-bar-item w3-button w3-padding-large">22</a>
+    <a href="nick_23.php"class="w3-bar-item w3-button w3-padding-large">23</a>
+    <a href="nick_24.php"class="w3-bar-item w3-button w3-padding-large">24</a>
+    <a href="nick_25.php"class="w3-bar-item w3-button w3-padding-large">25</a><br>
+    <a href="nick_26.php"class="w3-bar-item w3-button w3-padding-large">26</a>
+    <a href="nick_27.php"class="w3-bar-item w3-button w3-padding-large">27</a>
+    <a href="nick_28.php"class="w3-bar-item w3-button w3-padding-large">28</a>
+    <a href="nick_29.php"class="w3-bar-item w3-button w3-padding-large">29</a>
+    <a href="nick_30.php"class="w3-bar-item w3-button w3-padding-large">30</a><br>
+    <a href="nick_31.php"class="w3-bar-item w3-button w3-padding-large">31</a>
+    <a href="nick_32.php"class="w3-bar-item w3-button w3-padding-large">32</a>
+    <a href="nick_33.php"class="w3-bar-item w3-button w3-padding-large">33</a>
+    <a href="nick_34.php"class="w3-bar-item w3-button w3-padding-large">34</a>
+    <a href="nick_35.php"class="w3-bar-item w3-button w3-padding-large">35</a><br>
+    <a href="nick_36.php"class="w3-bar-item w3-button w3-padding-large">36</a>
+    </table>
+<footer class="w3-container w3-padding-64 w3-center w3-opacity w3-light-grey w3-xlarge">
+  <a href="https://www.facebook.com/Nick.Godzilla?ref=bookmarks"><i class="fa fa-facebook-official w3-hover-opacity"></i></a>
+  <a href="https://www.instagram.com/nickkybeginner/?hl=th"><i class="fa fa-instagram w3-hover-opacity"></i></a>
+  <a href="https://www.youtube.com/channel/UC7RFNDhNZD4x_UvToRIRNmA"><i class="fa fa-youtube w3-hover-opacity"></i></a>
+  <a href="https://twitter.com/55120Titan"><i class="fa fa-twitter w3-hover-opacity"></i></a>
+</footer>
+</body>
+</html>
+
+   
+
+    
+
+
+
