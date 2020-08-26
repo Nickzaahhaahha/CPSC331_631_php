50bed4aad18207e0c539d1650189d8d10ffb29f4
Suriya005 <45837558+Suriya005@users.noreply.github.com>
2020-08-26 23:42:46 +0700

    Add files via upload

---
+++ link3page/index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<style>
+    body {
+        font-family: "Lato", sans-serif;
+        background-color: #010101;
+        
+    }
+    .mySlides {display: none}
+</style>
+</head>
+<body>
+<div class="w3-top">
+  <div class="w3-bar w3-black w3-card">
+    <a href="index.php" class="w3-bar-item w3-button w3-padding-large">หน้าแรก</a>
+    <a href="one.php" class="w3-bar-item w3-button w3-padding-large w3-hide-small">PHPinfo</a>
+    <a href="two.php" class="w3-bar-item w3-button w3-padding-large w3-hide-small">Pyramid</a>
+    <a href="three.php" class="w3-bar-item w3-button w3-padding-large w3-hide-small">iframe</a>
+  </div>
+</div>
+<section>
+<header class="w3-display-container w3-content w3-wide" style="max-width:1500px;" id="home">
+  <img class="w3-image" src="https://i.pinimg.com/originals/30/94/e0/3094e0fd1114787639e8e334a840ca02.jpg" alt="Architecture" width="1500" height="800">
+  <div class="w3-display-middle w3-margin-top w3-center">
+    <h1 class="w3-xxlarge w3-text-white"><span class="w3-padding w3-black w3-opacity-min"><b>NICK</b></span> <span class="w3-hide-small w3-text-light-grey">Suriya</span></h1>
+  </div>
+</header>
+
+
+
+
+</section>
+<footer class="w3-container w3-padding-64 w3-center w3-opacity w3-light-grey w3-xlarge">
+  <a href="https://www.facebook.com/Nick.Godzilla?ref=bookmarks"><i class="fa fa-facebook-official w3-hover-opacity"></i></a>
+  <a href="https://www.instagram.com/nickkybeginner/?hl=th"><i class="fa fa-instagram w3-hover-opacity"></i></a>
+  <a href="https://www.youtube.com/channel/UC7RFNDhNZD4x_UvToRIRNmA"><i class="fa fa-youtube w3-hover-opacity"></i></a>
+  <a href="https://twitter.com/55120Titan"><i class="fa fa-twitter w3-hover-opacity"></i></a>
+</footer>
+</body>
+</html>
---
+++ link3page/three.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<style>
+    body {
+        font-family: "Lato", sans-serif;
+        background-color: #010101;
+        
+    }
+    .mySlides {display: none}
+</style>
+</head>
+<body>
+<section>
+<div class="w3-top">
+  <div class="w3-bar w3-black w3-card">
+    <a href="index.php" class="w3-bar-item w3-button w3-padding-large">หน้าแรก</a>
+    <a href="one.php" class="w3-bar-item w3-button w3-padding-large w3-hide-small">PHPinfo</a>
+    <a href="two.php" class="w3-bar-item w3-button w3-padding-large w3-hide-small">Pyramid</a>
+    <a href="three.php" class="w3-bar-item w3-button w3-padding-large w3-hide-small">iframe</a>
+  </div>
+</div>
+</section>
+  <br><br>
+<iframe class= "iframe1" src="http://www.facebook.com/plugins/likebox.php?href=http%3A%2F%2Fwww.facebook.com%2Ftechhub.arip%3Fref%3Dsgm%23%21%2Ftechhub.arip%3Fref%3Dsgm&width=400&connections=9&stream=true&header=true&height=550" 
+scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:300px; height:550px; align : center;" allowTransparency="true"></iframe>
+<section>
+<br>    
+
+    
+    </section>
+    <footer class="w3-container w3-padding-64 w3-center w3-opacity w3-light-grey w3-xlarge">
+  <a href="https://www.facebook.com/Nick.Godzilla?ref=bookmarks"><i class="fa fa-facebook-official w3-hover-opacity"></i></a>
+  <a href="https://www.instagram.com/nickkybeginner/?hl=th"><i class="fa fa-instagram w3-hover-opacity"></i></a>
+  <a href="https://www.youtube.com/channel/UC7RFNDhNZD4x_UvToRIRNmA"><i class="fa fa-youtube w3-hover-opacity"></i></a>
+  <a href="https://twitter.com/55120Titan"><i class="fa fa-twitter w3-hover-opacity"></i></a>
+</footer>
+</body>
+</html>
