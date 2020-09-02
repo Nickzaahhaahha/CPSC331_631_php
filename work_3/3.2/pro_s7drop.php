d83d8645c7041ef9f054a40913ce9442fabaadc5
Suriya005 <45837558+Suriya005@users.noreply.github.com>
2020-09-03 00:44:43 +0700

    ww

--- work_3/3.2/pro_s7drop.php
+++
@@ -1,9 +0,0 @@
-<?php
-require("s1connect.php");
-if (!$result=mysql_query("drop database $db",$connect))
-  echo "Drop database : failed";
-else
-  echo "Drop database : succeeded";
-mysql_close($connect);
-?>
-<input type='button' onclick="location.href='s0index.php'" value='back'>
