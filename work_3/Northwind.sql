142be5aca29e9437f1ec3bb8091f0cd6b982a1bb
Suriya005 <45837558+Suriya005@users.noreply.github.com>
2020-09-03 00:46:52 +0700

    Update Northwind.sql

--- work_3/Northwind.sql
+++ work_3/Northwind.sql
@@ -10,7 +10,7 @@
 /*!50503 SET NAMES utf8mb4 */;
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-
+/* create by nick za 007 */
 
 -- Dumping database structure for northwind_db
 CREATE DATABASE IF NOT EXISTS `northwind_db` /*!40100 DEFAULT CHARACTER SET utf8mb4 */;
